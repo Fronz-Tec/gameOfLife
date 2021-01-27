b803e8b5256aa54f655751317a0ab855bc63eb69
kfronzeck <kevin.fronzeck@obvita.ch>
2021-01-27 17:04:52 +0100

    UI Elements Start/Stop Buttons Generation Speed Slider Button for Showing Cell Age

--- src/ch/bbw/Main.java
+++ src/ch/bbw/Main.java
@@ -5,12 +5,20 @@ import ch.bbw.lib.file.Export;
 import ch.bbw.lib.file.Import;
 import ch.bbw.model.Cell;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.Random;
 
 public class Main extends Application {
@@ -28,29 +36,72 @@ public class Main extends Application {
         Cell[][] cell = new Cell[20][20];
 
         for (int x = 0; x < cell.length; x++) {
-            for(int y = 0; y < cell[x].length; y++){
+            for (int y = 0; y < cell[x].length; y++) {
                 cell[x][y] = new Cell();
                 cell[x][y].setNewState(new Random().nextBoolean());
                 cell[x][y].updateState();
-                Rectangle rectangle = new Rectangle(20,20,20,20);
+                Rectangle rectangle = new Rectangle(20, 20, 20, 20);
                 rectangle.setStroke(Color.GRAY);
                 rectangle.setFill(Color.WHITE);
-                gridPane.add(rectangle,x,y,1,1);
+                gridPane.add(rectangle, x, y, 1, 1);
             }
         }
 
-        Game game = new Game(cell);
+        Game game = new Game(cell,gridPane);
 
-        game.showGrid(gridPane);
-        game.calculateGeneration();
-        game.showGrid(gridPane);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(gridPane);
 
-        Export.exportIsAliveState(cell);
-        cell = Import.importIsAliveStates("");
+        //TODO 2 more buttons import Export
+        //Export.exportIsAliveState(cell);
+        //cell = Import.importIsAliveStates("");
 
-        Scene scene = new Scene(gridPane);
+        //TODO 1 Button Random
+        //Action
+//        for (int x = 0; x < cell.length; x++) {
+//            for (int y = 0; y < cell[x].length; y++) {
+//                cell[x][y].setNewState(new Random().nextBoolean());
+//                cell[x][y].updateState();
+//            }
+//        }
+
+        FlowPane flowPane = new FlowPane();
+        Button startButton = new Button("Start");
+        startButton.setOnAction(game.start());
+
+        Button endButton = new Button("Stop");
+        endButton.setOnAction(game.stop());
+
+        Button cellAgeButton = new Button("Show Cell Age");
+        cellAgeButton.setOnAction(game.showCellAge());
+
+        Slider slider = new Slider(0.1, 5, 1);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                game.setGenerationSpeed(newValue);
+            }
+        });
+
+        flowPane.setVgap(20);
+        flowPane.setHgap(20);
+        flowPane.getChildren().add(startButton);
+        flowPane.getChildren().add(endButton);
+        flowPane.getChildren().add(cellAgeButton);
+        flowPane.getChildren().add(new Label("Speed (0.1 - 1)"));
+        flowPane.getChildren().add(slider);
+
+        borderPane.setBottom(flowPane);
+
+        Scene scene = new Scene(borderPane);
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("Conway's Game Of Life");
         primaryStage.show();
+
+        game.showGrid();
     }
 }
--- src/ch/bbw/controller/Game.java
+++ src/ch/bbw/controller/Game.java
@@ -1,39 +1,106 @@
 package ch.bbw.controller;
 
 import ch.bbw.model.Cell;
-import ch.bbw.view.ConsoleView;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.util.Random;
 
 public class Game {
+    private Cell [][] grid;
+    private boolean showCellAge;
+    private int speed;
+    private Thread runnerThread;
+    private GridPane gridPane;
 
-    private ConsoleView consoleView;
+    private boolean pauseRunningThread;
 
     private int width = 20;
     private int height = 20;
 
-    private Cell [][] grid;
 
-    public Game(Cell[][] grid) {
+    public Game(Cell[][] grid, GridPane gridPane) {
         this.grid = grid;
+        this.gridPane = gridPane;
+        this.showCellAge = false;
+        this.pauseRunningThread = true;
+        this.speed = 1000;
         this.width = grid.length;
         this.height = grid.length;
-        this.consoleView = new ConsoleView();
+
+        runnerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    if (!pauseRunningThread) {
+                        try {
+                            Game.this.calculateGeneration();
+                            if (!Game.this.cellsChaning()) {
+                                Platform.runLater(() -> {
+                                    Stage dialog = new Stage();
+                                    dialog.initStyle(StageStyle.UTILITY);
+                                    Scene scene = new Scene(new Group(new Text(25, 25, "No more generations!")));
+                                    dialog.setScene(scene);
+                                    dialog.showAndWait();
+                                });
+                                pauseRunningThread = true;
+                            }
+                            Game.this.updateCells();
+                            Game.this.showGrid();
+                            Thread.sleep(speed);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+            }
+        });
+        runnerThread.start();
     }
 
-    public void showGrid(GridPane gridPane){
+    private boolean cellsChaning() {
+        boolean cellsChanged = false;
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+//                if (grid[x][y].isAlive() != grid[x][y].isNewState()) {
+//                    cellsChanged = true;
+//                }
+                if (grid[x][y].isOldState() != grid[x][y].isNewState()) {
+                    cellsChanged = true;
+                }
+            }
+        }
+        return cellsChanged;
+    }
+
+    public void showGrid(){
         //Clear Console doesn't work properly, using JavaFX instead
 //        consoleView.displayCells(grid);
 
         for (int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++){
                 if(grid[x][y].isAlive()){
-                    ((Rectangle)(gridPane.getChildren().get(x * grid.length + y))).setFill(Color.RED);
+                    if (showCellAge) {
+                        ((Rectangle)(gridPane.getChildren().get(x * grid.length + y))).setFill(Color.RED);
+                        if (grid[x][y].getAge() > 0) {
+                            ((gridPane.getChildren().get(x * grid.length + y))).setOpacity(1 / new Double(grid[x][y].getAge()));
+                        }
+                    } else {
+                        ((Rectangle)(gridPane.getChildren().get(x * grid.length + y))).setFill(Color.RED);
+                    }
                 }else{
                     ((Rectangle)(gridPane.getChildren().get(x * grid.length + y))).setFill(Color.WHITE);
+                    ((gridPane.getChildren().get(x * grid.length + y))).setOpacity(1);
                 }
 
             }
@@ -76,9 +143,6 @@ public class Game {
         if(hasNeighbor(row+1,collumn+1)){
             result++;
         }
-
-
-
 
         return result;
     }
@@ -150,4 +214,34 @@ public class Game {
         return grid[row][collumn];
     }
 
+    public EventHandler<ActionEvent> start() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                pauseRunningThread = false;
+            }
+        };
+    }
+
+    public EventHandler<ActionEvent> stop() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                pauseRunningThread = true;
+            }
+        };
+    }
+
+    public EventHandler<ActionEvent> showCellAge() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                showCellAge = !showCellAge;
+            }
+        };
+    }
+
+    public void setGenerationSpeed(Number newValue) {
+        speed = (int) (newValue.doubleValue()*1000);
+    }
 }
